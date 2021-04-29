bc98ef8379512340a9d3caff337de79da59e8a65
Meyssa Slama <maissa.slama@gmail.com>
2021-04-29 16:46:50 +0200

    hello there

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import imagesrc from "./imagesrc.png"
+import './style.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1 className="titel red">
+       Meyssa
+     </h1>
+     <div>
+       <br/>
+     </div>
+     <img src={imagesrc} alt='image'/>
+     <div>
+       <br/>
+     </div>
+     <img src='/imagepub.png'/>
+     <div>
+     <iframe width="320" height="240" src="https://www.youtube.com/embed/yTNE1ru9ELc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+     </div>
     </div>
   );
 }
